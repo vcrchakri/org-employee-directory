6f0ff7abe13abd0bf7cae52b1da3cce5759daffd
Chakri <vcrchakri@gmail.com>
2026-03-09 11:29:03 +0530

    fix static files and whitenoise

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -94,13 +94,16 @@ USE_TZ = True
 
 
 # Static files
+# Static files
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATICFILES_DIRS = [
     BASE_DIR.parent / 'frontend'
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
